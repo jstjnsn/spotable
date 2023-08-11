3ee5739ff72d02b3a0f7828adefa030515e4fc10
Joost Jansen <joost.jansen1@ns.nl>
2023-08-11 21:21:23 +0200

    prepare for production

--- server.js
+++ server.js
@@ -1,16 +1,48 @@
+import compression from "compression";
 import express from "express";
+import RateLimit from "express-rate-limit";
+import helmet from "helmet";
 import pgPromise from "pg-promise";
 
+// DATABASE
+
 const pgp = pgPromise({});
+
+const devDbUrl = "postgres://spots:spots@localhost:5432/spots";
+
+const dbUrl = process.env.DATABASE_URL || devDbUrl;
+
+// SERVER SETUP
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
+
+app.use(compression());
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'"],
+    },
+  })
+);
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 20,
+});
+
+app.use(limiter);
+
 app.set("view engine", "ejs");
 
-const PORT = 3000;
+// ROUTES
 
 app.get("/map", async (_, res) => {
   res.render("map", { title: "Map" });
@@ -18,7 +50,7 @@ app.get("/map", async (_, res) => {
 
 app.get("/my-spots", async (_, res) => {
   try {
-    const db = pgp("postgres://spots:spots@localhost:5432/spots");
+    const db = pgp(dbUrl);
     const spots = await db.any('select * from "spots"');
     res.render("my-spots", { spots, title: "My Spots" });
   } catch (error) {
@@ -40,6 +72,7 @@ app.post("/spots", async (req, res) => {
       throw new Error("One of the required fields is empty");
     }
 
+    const db = pgp(dbUrl);
     const newSpot = await db.one(
       'insert into "spots" (description, address) values ($1, $2) returning *',
       [spotDescription, spotAddress]
@@ -53,6 +86,10 @@ app.post("/spots", async (req, res) => {
     res.status(500).send("Internal server error: " + error);
   }
 });
+
+// START SERVER
+
+const PORT = 3000;
 
 app.listen(PORT, () =>
   console.log(`🚀 Server running on http://localhost:${PORT}`)
